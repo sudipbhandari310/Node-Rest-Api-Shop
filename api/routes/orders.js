ae123b375db16559cc05e3cf87ceb54fc7daab19
Sudip <sudipbhandari310@gmail.com>
2024-02-22 14:43:49 +0545

    product and order routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,17 +1,104 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const router = express.Router();
-
+const Order = require("../model/orderModel");
+const Product = require("../model/productModel");
 router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "Orders are fetched" });
+  Order.find()
+    .select("product quantity _id")
+    .populate("product", "name")
+    .exec()
+    .then((result) => {
+      res.status(201).json({
+        count: result.length,
+        orders: result.map((result) => {
+          return {
+            _id: result._id,
+            product: result.product,
+            quantity: result.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders/" + result._id,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+  // res.status(200).json({ message: "Orders are fetched" });
 });
 
 router.post("/", (req, res, next) => {
-  res.status(201).json({ message: "Order has been created" });
+  Product.findById(req.body.productId)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      const order = new Order({
+        // _id: mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId,
+      });
+
+      return order.save();
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Order has been stored",
+        product: result,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/" + result._id,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get("/:orderId", (req, res, next) => {
-  res.json({ message: "Order details", id: req.params.orderId });
+  Order.findById(req.params.orderId)
+    .populate("product")
+    .exec()
+    .then((order) => {
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+      res.status(201).json({
+        order: order,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders",
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+router.delete("/:orderId", (req, res, next) => {
+  Order.deleteOne({ _id: req.params.orderId })
+    .exec()
+    .then((item) => {
+      res.status(200).json({
+        message: "Item deleted",
+        request: {
+          type: "POST",
+          url: "http://localhost:3000/orders",
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+  // res.json({ message: "Order deleted", id: req.params.orderId });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://sudip:test123@node-rest-shop.v1fneqb.mongodb.net/shop"
+  // { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use(morgan("dev")); // logs every thing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin , X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE,PATCH");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
+
   error.status = 404;
   next(error);
 });
